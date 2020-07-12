cab696bd602d065363a89e7abd4d6accb26f9068
Allen <noc@vonnue.com>
2020-07-13 02:36:01 +0530

    making comments more clearer

--- 99_wordanim/app.js
+++ 99_wordanim/app.js
@@ -4,17 +4,17 @@
 const text = document.querySelector(".fancy");
 const stringText = text.textContent;
 
-/* split the text content string into an
- * array of characters which we can animate
+/* Split the text content string into an array of characters 
+ * which we can animate by adding css properites.
  */
 const splitText = stringText.split("");
 
-/* Reset the text content to avoid duplication
- * when the animation is rendered
+/* Reset the text content to avoid text duplication when the 
+ * animation effect is added later.
  */ 
 text.textContent = "";
 
-/* loop through each character and make it into a span */
+/* Loop through each character and make it into a span */
 for(let i=0; i < splitText.length; i++) {
     if (splitText[i] === " ") {
         text.innerHTML += "<span>" + "&nbsp" + "</span>";
@@ -25,18 +25,19 @@ for(let i=0; i < splitText.length; i++) {
 
 /* Timing code */
 let char = 0;
+
 /* This tells the browser to call the function onTick
  * every 50 ms; try increasing the callback timer and
  * see how the output varies
  */
 let timer = setInterval(onTick, 50);
 
-/* This function will iterate through the spans
- * and the fade class to each span element, as the
- * function is called every 50 ms we get transition
- * effect. The variable char is incremented during
- * each invocation and the timer is cleared once
- * the end of the string condition is reached.
+/* This function will iterate through the spans and add 
+ * the "fade" class to each span element. As the function 
+ * is called every 50 ms we get a transition effect. The 
+ * variable "char" is incremented during each invocation 
+ * and the timer is cleared once the end of the string 
+ * condition is reached.
  */
 function onTick() {
     const span = text.querySelectorAll('span')[char];
